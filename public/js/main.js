72e6d9961c4d88c6a70f9ed8f9d8675d1193bf59
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2020-04-01 23:40:24 -0500

    required
    Just to see the world on fire

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,21 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+//Close when click (x)
+var close = document.getElementsByClassName("closeC")[0];
+
+//Get the Logo element that close when open modal
+var logo = document.getElementsByClassName("logo")[0];
+
+//Get the icon element
+
+var icon = document.getElementsByClassName("material-icons")[0];
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+  logo.style.display = "none";
+  icon.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -25,11 +37,18 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+close.onclick = function() {
+  logo.style.display = "block";
+  icon.style.display = "block";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
     document.getElementById('textC').value = "";
+    logo.style.display = "block";
+    icon.style.display = "block";
   }
 }
 
@@ -45,5 +64,7 @@ $(document).keyup(function(e) {
      // <DO YOUR WORK HERE>
      modal.style.display = "none";
      document.getElementById('textC').value = "";
+     logo.style.display = "block";
+     icon.style.display = "block";
  }
 });
