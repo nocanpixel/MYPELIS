2d53969056f98ab7c0e3c207f2de73ae53b3ba59
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2020-04-05 22:05:19 -0500

    let see

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 const mysql = require('mysql');
-let connection;
 
+
+const connection = mysql.createPool({
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b091c9ca29261a',
+    password: '0a9310fa',
+    database: 'heroku_4a2777817a2812a',
+    multipleStatements: true
+});
+
+
+
+/*
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -12,12 +23,14 @@ if (process.env.JAWSDB_URL) {
     multipleStatements: true
 });
 
-/*
 b91c9568152bce:b0f3c3ae
 
 @us-cdbr-iron-east-01.cleardb.net/heroku_17070a22fd3a03e
-*/
+
 
 }
+*/
+
+
 connection.connect();
 module.exports = connection;
