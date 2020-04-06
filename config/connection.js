ca9a7bcb25670fb33ebf4bbbf0c7ad8670c1ae74
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2020-04-05 21:04:50 -0500

    Connection

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,18 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'loquendo_1',
-    database: 'mypelis_db',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b091c9ca29261a',
+    password: '0a9310fa',
+    database: 'heroku_4a2777817a2812a',
     multipleStatements: true
 });
+
+/*
+b91c9568152bce:b0f3c3ae
+
+@us-cdbr-iron-east-01.cleardb.net/heroku_17070a22fd3a03e
+*/
 
 }
 connection.connect();
